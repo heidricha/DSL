14c473f2e74b73e7df54c47653a7b7143b5d3138
Heidrich Attila Admin <attila.heidrich@eli.alps.hu>
2016-10-04 14:51:19 +0200

    initial

---
+++ telnet.py
@@ -0,0 +1,15 @@
+from telnetlib import Telnet
+from time import sleep
+
+try:
+    tn = Telnet('127.0.0.1', port=25, timeout=5)
+except Exception as ex:
+    print(ex)
+assert tn
+
+print(tn.read_very_eager())
+tn.write('ehlo localhost\n')
+print(tn.read_very_eager())
+tn.write('quit\n')
+print(tn.read_all())
+tn.close
